764296ec8a03957bb8731a43d9f1c0819f99c645
Gabriel Külzer <gabrielk.rodrigues@hotmail.com>
2021-07-28 19:58:43 -0300

    Projeto pokedex

---
+++ android/app/src/main/kotlin/br/com/gabrielkulzer/pokedex/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.gabrielkulzer.pokedex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home/widgets/weaknesses.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/screens/home/widgets/list_widgets.dart';
+
+class Weaknesses extends StatelessWidget {
+  final List<String> listOfWeaknesses;
+  Weaknesses(this.listOfWeaknesses);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20, bottom: 10),
+            child: Text(
+              "Fraquezas",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: buildList(listOfWeaknesses),
+          )
+        ],
+      ),
+    );
+  }
+}
